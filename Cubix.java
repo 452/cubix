bfa5f2a2235eaf97c5a83b42e261835d29b475e0
Kin F. Tam <kft201@nyu.edu>
2012-11-20 09:34:13 -0500

    Shared binary image and audio folder created

--- Cubix.java
+++ Cubix.java
@@ -21,7 +21,6 @@ public class Cubix extends RenderApplet
 	Polygon maxColorGameButton;
 	Color buttonColor;
 
-	GameCube cube;
 	Game game;
 
 	// Gameplay variables
@@ -30,10 +29,10 @@ public class Cubix extends RenderApplet
 
 	public void initialize()
 	{
-		
+
 		//String imagePath = getCodeBase()+"background.png";
-		Renderer.setBg("background.png");
-		
+		Renderer.setBg("images/background1.png");
+
 		// Initialize world color and light variables
 		//setBgColor(.7, .7, .9);
 		addLight( 1, 1, 1, .8, .85, 1);
@@ -89,7 +88,7 @@ public class Cubix extends RenderApplet
 			this.game = new games.maxColorGame.MaxColorGame(this, this.getWorld(), this.audioOn);
 			this.game.initGame();
 		}
-		
+
 	}
 
 	private void stopGame()
@@ -117,7 +116,7 @@ public class Cubix extends RenderApplet
 
 	public void drawOverlay(Graphics g)
 	{
-		
+
 		// Draw the audio button regardless of status
 		g.setColor(Color.WHITE);
 		g.fillPolygon(this.audioButton);
@@ -125,7 +124,7 @@ public class Cubix extends RenderApplet
 		g.drawString(this.audioOn ? "AUDIO: ON" : "AUDIO: OFF", this.audioButton.getBounds().x+10,
 			this.audioButton.getBounds().y+20);
 
-		
+
 		// If on the intro screen, draw it
 		if(this.introScreenOn)
 		{
