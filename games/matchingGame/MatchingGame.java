354f602f698b52a7007d1e06d5a46a88bd6f47b4
luoyang2047 <luoyang2047@gmail.com>
2012-12-10 14:56:06 -0500

    Update games/matchingGame/MatchingGame.java
    adding some visual on the side

--- games/matchingGame/MatchingGame.java
+++ games/matchingGame/MatchingGame.java
@@ -2,6 +2,7 @@ package games.matchingGame;
 
 import games.*;
 import elements.*;
+
 import render.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -14,18 +15,27 @@ public class MatchingGame extends Game implements ActionListener
 
 	private int level = 1, numCubeDimensions = 3;
 
+	int effectTime1 = 0;
+	int effectTime2 = 0;
+	int timer = 0;
 	// Gameplay variables
 	private boolean gameOn = false, gameOver = false, gameWin = false;
 
+	private MatchingCube temp;
 	// Audio variables
 	private AudioClip backgroundAudio;
 	private boolean audioOn = false;
+	
+	
 
 	public MatchingGame(java.applet.Applet app, Geometry world, boolean audioStatus)
 	{
 		this.app = app;
 		this.world = world;
 		this.audioOn = audioStatus;
+		
+		this.backgroundAudio = this.app.getAudioClip(this.app.getCodeBase(),
+			"audio/background1.wav");
 	}
 
 	public void initGame()
@@ -59,14 +69,40 @@ public class MatchingGame extends Game implements ActionListener
 			this.deleteCubeFromWorld();
 		}
 		// Re-create the cube
-		this.cube = new MatchingCube(world, this.numCubeDimensions);
+		if(this.level == 1){
+			this.cube = new MatchingCube(world, this.numCubeDimensions);
+		}
+		else{
+			this.cube = new MatchingCube(world, this.numCubeDimensions + 1);
+		}
 		// Call method to randomize the cube tile colors
 		((MatchingCube)(this.cube)).randomizeCubeColors();
 	}
 
 	public void clickTile(int face, int row, int column)
 	{
-		((MatchingCube)(this.cube)).actionAt(face, row, column);
+		if(this.gameWin){
+			this.initLevel();
+			this.gameWin = false;
+			this.gameOver = false;
+		}
+		else{	
+			boolean isWin = ((MatchingCube)(this.cube)).actionAt(face, row, column);	
+			if(isWin){
+				this.gameOver = true;
+				this.gameWin = true;
+				if(this.level == 1)
+					this.level = 2;
+				else
+					this.level = 1;
+			}
+		}
+		if(((MatchingCube)(this.cube)).clickTime % 2 == 1){
+			this.effectTime1 = 0;
+		}
+		else{
+			this.effectTime2 = 0;
+		}
 	}
 
 	public void toggleAudio(boolean b)
@@ -82,7 +118,7 @@ public class MatchingGame extends Game implements ActionListener
 		if(this.backgroundAudio == null)
 		{
 			this.backgroundAudio = this.app.getAudioClip(this.app.getCodeBase(),
-				"audio/background2.wav");
+				"audio/background1.wav");
 		}
 
 		this.backgroundAudio.loop();
@@ -103,37 +139,56 @@ public class MatchingGame extends Game implements ActionListener
 		}
 	}
 
-	Color tempColor;
-	Font tempFont;
-
+	public void rectEffect1(Graphics g){
+		g.fillRect((int)(120 - 60 * Math.sin((double)effectTime1 * Math.PI / 180)), (int)(140 - 60 * Math.sin((double)effectTime1 * Math.PI / 180))
+				, (int)(120 * Math.sin((double)effectTime1 * Math.PI / 180)), (int)(120 * Math.sin((double)effectTime1 * Math.PI / 180)));
+		if(this.effectTime1 != 120){
+			this.effectTime1 +=10;
+		}
+	}
+	
+	public void rectEffect2(Graphics g){
+		g.fillRect((int)(120 - 60 * Math.sin((double)effectTime2 * Math.PI / 180)), (int)(280 - 60 * Math.sin((double)effectTime2 * Math.PI / 180))
+				, (int)(120 * Math.sin((double)effectTime2 * Math.PI / 180)), (int)(120 * Math.sin((double)effectTime2 * Math.PI / 180)));
+		if(this.effectTime2 != 120){
+			this.effectTime2 +=10;
+		}
+	}
+	
 	public void drawOverlay(Graphics g)
 	{
-		// Store font and color already in the system
-		this.tempColor = g.getColor();
-		this.tempFont = g.getFont();
-
-		// If game is over, draw the message
 		if(this.gameOver)
 		{
 			g.setColor(Color.RED);
 			g.setFont(Fonts.BIG_FONT);
-			g.drawString("GAME OVER", 220, 200);
 			g.drawString((this.gameWin ? "YOU WIN!" : "YOU LOSE!"), 240, 250);
 		}
-
+		
+		g.setColor(Color.BLUE);
+		g.setFont(Fonts.BIG_FONT);
+		g.drawString("Level " + this.level, 30, 400);
+		
+		this.temp = (MatchingCube)this.cube;
+		String s = "Find the matching color...";
+		if(this.temp.getOneColor() != null){
+			g.setColor(this.temp.getOneColor());
+			//g.fillRect(30, 200, (int)(100 * Math.sin(Math.PI / 2)), (int)(100 * Math.sin(Math.PI / 2)));//center at 70, 240
+			this.rectEffect1(g);
+		}
+		if(this.temp.getTwoColor() != null){
+			g.setColor(this.temp.getTwoColor());
+			//g.fillRect(550, 200, (int)(100 * Math.sin(Math.PI / 2)), (int)(100 * Math.sin(Math.PI / 2)));// center at 590, 240
+			this.rectEffect2(g);
+			if(this.temp.getOneColor().equals(this.temp.getTwoColor())){
+				s = "Great! Keep on finding!";
+			}
+			else{
+				s = "Memorize the colors!";
+			}
+		}
 		g.setColor(Color.BLUE);
 		g.setFont(Fonts.SMALL_FONT);
-
-		// Draw the top level text
-		g.drawString("MATCHING GAME SUPREMUS", 170, 30);
-
-		g.setFont(Fonts.TINY_FONT);
-		g.drawString("Click on two unexposed black tiles to expose their colors", 150, 45);
-		g.drawString("Click on a third unexposed tile to complete the match or close out the tiles", 100, 60);
-		g.drawString("Match all colored tiles and you win!", 210, 75);
-
-		g.setFont(tempFont);
-		g.setColor(tempColor);
+		g.drawString(s, 200, 80);
 	}
 
 	// Action to perform - ActionListener implementation
